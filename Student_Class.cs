f172ac6ee8db4b40915dd308059e8625c246941c
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-08-31 12:49:26 +0300

    Add partial class Program

---
+++ Student_Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+namespace Structure_Classes_Properties
+{
+    partial class Program
+    {
+        static void Main5()
+        {
+            Student student = new Student("Олег", 22, "П-202");
+
+            student.EnterMarks();
+            WriteLine();
+            student.StudentInfoPrint();
+
+        }
+    }
