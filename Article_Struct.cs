b351144d1c1e91fd7691f51ef0c321a55b13b015
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-08-31 12:43:24 +0300

    Add struct Article

---
+++ Article_Struct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Structure_Classes_Properties
+{
+
+    struct Article
+    {
+        private int codeProduct;
+        private string nameProduct;
+        public double priceProduct;
+        private ArticleType articleType;
+        public enum ArticleType
+        {
+            Foods, Clothes, Furniture, Gadget
+        }
+        public Article(int codeProduct, string nameProduct, double priceProduct, ArticleType articleType)
+        {
+            this.codeProduct = codeProduct;
+            this.nameProduct = nameProduct;
+            this.priceProduct = priceProduct;
+            this.articleType = articleType;
+        }
+
+        public void Print()
+        {
+            WriteLine($"код товара :{codeProduct}  \nназвание товара: { nameProduct}  \nценa товара: { priceProduct} \nтип товарa:{ articleType}");
+        }
+        public void PrintProduct()
+        {
+            WriteLine($"название товара: { nameProduct}");
+        }
+    }
+}
