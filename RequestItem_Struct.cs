03c45f0ecb1c73219a316ab8457dcf59a0719410
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-08-31 12:46:08 +0300

    Add struct RequestItem

---
+++ RequestItem_Struct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Structure_Classes_Properties
+{
+
+    struct RequestItem
+    {
+
+        public int count;
+        public Article ar;
+
+        public RequestItem(Article ar, int count)
+        {
+
+            this.count = count;
+            this.ar = ar;
+        }
+
+
+        public void Print()
+        {
+            ar.Print();
+            WriteLine($"количество единиц товара: {count}\n\n");
+        }
+    }
+}
