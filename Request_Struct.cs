1829f0e378e24ab8cba0217400df11bc95d1405f
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-08-31 12:47:33 +0300

    Add struct Request

---
+++ Request_Struct.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using System.Linq;
+
+namespace Structure_Classes_Properties
+{
+
+    struct Request
+    {
+        private int codeOrder;
+        private Client client;
+        private DateTime dateOrder;
+        private List<RequestItem> requestItemList;
+        private PayType pay;
+        public enum PayType
+        {
+            Cash, Check, Credit
+        }
+        public Request(int codeOrder, Client client, Request.PayType pay, List<RequestItem> requestItemList)
+        {
+            this.codeOrder = codeOrder;
+            this.client = client;
+            this.pay = pay;
+            this.dateOrder = DateTime.Now;
+            this.requestItemList = requestItemList;
+        }
+        public double Sum
+        {
+            get
+            {
+                double Sum = 0;
+                foreach (RequestItem item in requestItemList)
+                {
+
+                    Sum += item.count * item.ar.priceProduct;
+                }
+                return Sum;
+            }
+        }
+        public void Print()
+        {
+            WriteLine($"код заказа:{codeOrder} \nклиент: {client.Get()} \nдата заказа: {dateOrder.ToString("yyyy /MM/dd")} \nформa оплаты: { pay}");
+
+            foreach (RequestItem requestItem in requestItemList)
+            {
+
+                Console.WriteLine("{0}", requestItem);
+            }
+
+            WriteLine($"сумма заказа : {Sum}");
+        }
+    }
+}
