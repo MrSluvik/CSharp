a769edb009fb7c3947beea7c0fc168c46383a3cd
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-08-31 12:56:34 +0300

    Add partial class Program

---
+++ Seven_Wonders_World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SevenWonders;
+
+namespace Structure_Classes_Properties
+{
+    partial class Program
+    {
+        static void Main7()
+        {
+
+            Wonder1.Print();
+            Wonder2.Print();
+            Wonder3.Print();
+            Wonder4.Print();
+            Wonder5.Print();
+            Wonder6.Print();
+            Wonder7.Print();
+
+        }
+    }
+}
