becc06ed93589f540f1dc75d71042631245fb334
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-09-10 20:22:15 +0300

    створив екземляри класу Magazine and Shop і показав перегружені оператори

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using static System.Console;
 
-namespace Imitation
+namespace OperatorOverload
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Magazine magazine = new Magazine("Умійко", "А-ба-ба-га-ла-ма-га", 10000, "Дитячий", true, 400);
+            Magazine magazine2 = new Magazine("Водограй", "А-ба-ба-га-ла-ма-га", 10000, "Дитячий", true, 500);
+
+            magazine += 10;
+            WriteLine(magazine.numb_Employees);
+            magazine -= 20;
+            WriteLine(magazine.numb_Employees);
+            WriteLine($"{ magazine == magazine2}");
+            WriteLine($"{magazine != magazine2}");
+            WriteLine($"{magazine < magazine2}");
+            WriteLine($"{magazine > magazine2}");
+            WriteLine($" {magazine.ToString()}"); WriteLine();
+            WriteLine($" {magazine2.ToString()}");
+            WriteLine();
+
+            Shop shop = new Shop("Book", "м.Київ,вул.Щастя,буд.45", "Крутий опис",  70);
+            Shop shop2 = new Shop("World of Books", "м.Житомир,вул.Див,буд.7", "Ще крутіший опис", 90);
+
+            shop += 10;
+            WriteLine(shop.area);
+            shop -= 20;
+            WriteLine(shop.area);
+            WriteLine($"{ shop == shop2}");
+            WriteLine($"{shop != shop2}");
+            WriteLine($"{shop < shop2}");
+            WriteLine($"{shop > shop2}");
+            WriteLine($"{ shop.ToString()}"); WriteLine();
+             WriteLine($"{shop2.ToString()}");
         }
     }
 }
