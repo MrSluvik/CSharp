fdcc3b241a41b6ac9569f1b9b84d6ca3aa2c337c
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-09-10 22:35:45 +0300

    Add class ListBooks

--- Books.cs
+++ Books.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Math;
+using static System.Array;
 
 namespace OperatorOverload
 {
     class Books
     {
         public string Name { get; set; }
+        public Books()
+        {
+            Name = "";
+        }
         public Books(string Name)
         {
             this.Name = Name;
@@ -14,6 +20,33 @@ namespace OperatorOverload
         public override string ToString() 
         {
             return $"Books Name: {Name}\n"; 
+        }
+    }
+    class ListBooks
+    {
+        private Books[] books;
+        public ListBooks(int size)
+        {
+            books = new Books[size];
+        }
+        public int Length 
+        { 
+            get { return books.Length; }         
+        }
+        public Books this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < books.Length)
+                {
+                    return books[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                books[index] = value;
+            }
         }
     }
 }
