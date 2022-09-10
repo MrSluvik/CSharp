73c680c8e1f800c17fbde4639280a233cac28956
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-09-10 20:27:14 +0300

    Revert "Add code of class Books"
    This reverts commit 16751003bc48cfeba4cee4f8f9094af1203b8f67.

--- Books.cs
+++ Books.cs
@@ -6,11 +6,5 @@ namespace OperatorOverload
 {
     class Books
     {
-        public string Name { get; set; }
-        public Books(string Name)
-        { 
-            this.Name = Name;
-        }
-        public override string ToString() { return $"Book`s information: {Name}\n"; }
     }
 }
