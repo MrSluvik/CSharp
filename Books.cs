655110831f41fe19e378dcb0014f31191ee05849
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-09-13 23:31:00 +0300

    add function Add,Delate,FindElement and Print. add indecsator and getter

--- Books.cs
+++ Books.cs
@@ -48,5 +48,41 @@ namespace OperatorOverload
                 books[index] = value;
             }
         }
+        public void Add(string Name, int index)
+        {
+            books.SetValue(new Books (Name), index);
+        }
+        public int FindElement(string Name)
+        {
+            int i = 0;
+            foreach (Books item in books)
+            {
+                if (item.Name ==Name) 
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+        public void Delete(string Name)
+        {
+            int index = FindElement(Name);
+            Array.Clear(books, index, 1);
+        }
+        public void Print()
+        {
+            for (int i = 0; i < books.Length; i++)
+            {
+                if (books[i] == null)
+                {
+                  continue;
+                }
+                else
+                {
+                    Console.WriteLine($"{books[i].Name}");
+                }
+            }       
+        }
     }
 }
